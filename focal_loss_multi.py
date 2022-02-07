4de11152017cbec33ac39b6afcab2ea91c60bbdc
Sofi-Mukhtar <sofimukhtar1@gmail.com>
2022-02-08 02:58:25 +0530

    Add files via upload
    USING THIS FUNCTION , IF YOU HAVE HIGH DATA IMBALANCE. You don't need to oversample/undersample your data. It gives more weights to the hard examples and less focus to the easy examples
    focal loss for multi category of multi label problem
             Focal loss for multi-class or multi-label problems
             Alpha is used to specify the weight of different categories/tags. The array size needs to be the same as the number of categories.
             When there is a skew between different categories/tags in your dataset, try applying this function as a loss.
        Usage:
         model.compile(loss=[multi_category_focal_loss1(alpha=[1,2,3,2], gamma=2)], metrics=["accuracy"], optimizer=adam)

---
+++ focal_loss_multi.py
@@ -0,0 +1,17 @@
+def multi_category_focal_loss1(alpha, gamma=2.0):
+    
+    epsilon = 1.e-7
+    alpha = tf.constant(alpha, dtype=tf.float32)
+    
+    #alpha = tf.constant_initializer(alpha)
+    gamma = float(gamma)
+    def multi_category_focal_loss1_fixed(y_true, y_pred):
+        y_true = tf.cast(y_true, tf.float32)
+        y_pred = tf.clip_by_value(y_pred, epsilon, 1. - epsilon)
+        y_t = tf.multiply(y_true, y_pred) + tf.multiply(1-y_true, 1-y_pred)
+        ce = -tf.log(y_t)
+        weight = tf.pow(tf.subtract(1., y_t), gamma)
+        fl = tf.matmul(tf.multiply(weight, ce), alpha)
+        loss = tf.reduce_mean(fl)
+        return loss
+    return multi_category_focal_loss1_fixed
